8ceeab83d155135d9ce1fb4ad4442be65ff100b1
André <AndreR@outlook.de>
2022-01-12 12:34:41 +0100

    Logic Tier sketch

---
+++ ppedv.Autovermietung/ppedv.Autovermietung.Logic/Core.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.Autovermietung.Model;
+
+namespace ppedv.Autovermietung.Logic
+{
+    public class Core
+    {
+
+
+        public Auto GetFastedCar(DateTime date)
+        {
+            var con = new Data.EFCore.EfContext();
+            
+            throw new NotImplementedException();
+        }
+
+        public void AddVermietung(Auto auto, DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Auto> GetAllAvailableCars(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
