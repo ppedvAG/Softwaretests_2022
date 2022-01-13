42f5825541311ce7afb0e721c65baf1c01575aed
André <AndreR@outlook.de>
2022-01-13 11:22:46 +0100

    Jetzt auch mit speichern

--- ppedv.Autovermietung/ppedv.Autovermietung.UI.WPF/ViewModel/AutoViewModel.cs
+++ ppedv.Autovermietung/ppedv.Autovermietung.UI.WPF/ViewModel/AutoViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ppedv.Autovermietung.Logic;
 using ppedv.Autovermietung.Model;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace ppedv.Autovermietung.UI.WPF.ViewModel
 {
@@ -9,12 +10,16 @@ namespace ppedv.Autovermietung.UI.WPF.ViewModel
         public ObservableCollection<Auto> AutosList { get; set; }
         public Auto SelectedAuto { get; set; }
 
+        public ICommand SaveCommand { get; set; }
+
         private Core _core;
 
         public AutoViewModel()
         {
             _core = new Core(new Data.EFCore.EfRepository());
             AutosList = new ObservableCollection<Auto>(_core.Repository.GetAll<Auto>());
+
+            SaveCommand = new RelayCommand(x => _core.Repository.SaveChanges());
         }
     }
 }
