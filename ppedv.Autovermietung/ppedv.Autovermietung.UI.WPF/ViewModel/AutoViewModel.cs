962d47e56b5d8f81fa3f644a0869dc7a7b4fd446
André <AndreR@outlook.de>
2022-01-13 11:16:43 +0100

    WPF MVVM super oll

---
+++ ppedv.Autovermietung/ppedv.Autovermietung.UI.WPF/ViewModel/AutoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ppedv.Autovermietung.Logic;
+using ppedv.Autovermietung.Model;
+using System.Collections.ObjectModel;
+
+namespace ppedv.Autovermietung.UI.WPF.ViewModel
+{
+    public class AutoViewModel
+    {
+        public ObservableCollection<Auto> AutosList { get; set; }
+        public Auto SelectedAuto { get; set; }
+
+        private Core _core;
+
+        public AutoViewModel()
+        {
+            _core = new Core(new Data.EFCore.EfRepository());
+            AutosList = new ObservableCollection<Auto>(_core.Repository.GetAll<Auto>());
+        }
+    }
+}
